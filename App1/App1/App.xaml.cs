6e5a09d362bfb1fe488133247c324ec93dd4464e
AMW <73808213+AntonTech420@users.noreply.github.com>
2021-11-27 14:53:40 +1300

    Add project files.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using App1.Data;
+using App1.Model;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+       /* static CryptoDatabase database;*/
+
+        // Create the database connection as a singleton.
+        /*public static CryptoDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new CryptoDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }*/
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RestSharp;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string apiKey = "B8A8313A-F405-461D-8D2E-551C814FB74F";
+        private string baseImageUrl = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/";
+        private string extraAdd = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var client = new RestClient("https://rest.coinapi.io/v1/assets?filter_asset_id=BTC;ETH;XMR;LTC;USDT" + extraAdd);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-CoinAPI-Key", apiKey);
+            IRestResponse response = client.Execute(request);
+
+        }
+
+       
+    }
+}
